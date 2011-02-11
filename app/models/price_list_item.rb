0abb5369da5fb7e67b130edfde3a11ba01cf03e3
Doug Youch <doug@cykod.com>
2011-02-11 10:54:21 -0500

    Added extra prices model

--- app/models/price_list_item.rb
+++ app/models/price_list_item.rb
@@ -7,6 +7,10 @@ class PriceListItem < DomainModel
   
   acts_as_list :column => :position, :scope => :price_list_section_id
 
+  attr_accessor :extra_prices_text
+  
+  serialize :extra_prices
+  
   def child_sections; []; end
 
   def next_position
@@ -16,5 +20,39 @@ class PriceListItem < DomainModel
   def before_create
     self.price_list_menu_id ||= self.price_list_section.price_list_menu_id
     self.position ||= self.next_position
+    self.extra_prices ||= []
+  end
+  
+  def extra_prices_models
+    @extra_prices_models ||= self.extra_prices.collect { |p| Price.new(p) }
+  end
+  
+  def extra_prices_list
+    self.extra_prices_models.collect { |m| m.to_s }.join("\n")
+  end
+  
+  def extra_prices_list=(str)
+    self.extra_prices = str.split("\n").collect do |line|
+      price, name = line.split '::'
+      price = price.strip if price
+      name = name.strip if name
+      name = nil if name.blank?
+      price = nil if price.blank?
+      price ? Price.new(:price => price, :name => name).to_h : nil
+    end.compact
+  end
+  
+  class Price < HashModel
+    attributes :price => nil, :name => nil
+    
+    def to_s
+      if self.name
+        "#{self.price}::#{self.name}"
+      elsif self.price
+        "#{self.price}"
+      else
+        ''
+      end
+    end
   end
 end
--- spec/models/price_list_item_spec.rb
+++ spec/models/price_list_item_spec.rb
@@ -19,6 +19,28 @@ describe PriceListItem do
       @item.id.should_not be_nil
       @item.position.should_not be_nil
       @item.price_list_menu_id.should_not be_nil
+      @item.extra_prices.should == []
+    end
+    
+    describe "Price Lists" do
+      before(:each) do
+        @item = Factory.create(:price_list_item, :price_list_section => @section)
+      end
+      
+      it "should be able to store extra prices" do
+        @item.extra_prices_list = "7::steak\r\n8::chicken\n\n5\n 2 :: lamb\n\n"
+        @item.extra_prices.should == [{:price => '7', :name => 'steak'}, {:price => '8', :name => 'chicken'}, {:price => '5', :name => nil}, {:price => '2', :name => 'lamb'}]
+        @item.extra_prices_models.size.should == 4
+        @item.extra_prices_models[0].price.should == '7'
+        @item.extra_prices_models[0].name.should == 'steak'
+        @item.extra_prices_models[3].price.should == '2'
+        @item.extra_prices_models[3].name.should == 'lamb'
+        @item.save.should be_true
+        
+        @item = PriceListItem.find @item.id
+        @item.extra_prices.should == [{:price => '7', :name => 'steak'}, {:price => '8', :name => 'chicken'}, {:price => '5', :name => nil}, {:price => '2', :name => 'lamb'}]
+        @item.extra_prices_list.should == "7::steak\n8::chicken\n5\n2::lamb"
+      end
     end
   end
 end
